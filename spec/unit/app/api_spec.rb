004629ba83758b78a4369bb887f7d45cd21ea8d7
Clasher370 <tkasha89@gmail.com>
2018-03-19 22:41:44 +0300

    init

---
+++ spec/unit/app/api_spec.rb
@@ -0,0 +1,105 @@
+require_relative "../../../app/api"
+require "rack/test"
+
+module ExpenseTracker
+  RSpec.describe API do
+    include Rack::Test::Methods
+
+    def app
+      API.new(ledger: ledger)
+    end
+
+    def parsed
+      JSON.parse(last_response.body)
+    end
+
+    let(:ledger) { instance_double("ExpenseTracker::Ledger") }
+
+    describe "POST /expenses" do
+      context "when the expense is successfully recorded" do
+        let(:expense) { { "some" => "data" } }
+
+        before do
+          allow(ledger).to receive(:record)
+            .with(expense)
+            .and_return(RecordResult.new(true, 417, nil))
+        end
+
+        it "returns expense id" do
+          post "/expenses", JSON.generate(expense)
+
+          expect(parsed).to include("expense_id" => 417)
+        end
+
+        it "response with a 200 (OK)" do
+          post "/expenses", JSON.generate(expense)
+
+          expect(last_response.status).to eq(200)
+        end
+      end
+
+      context "when the expense is fails validation" do
+        let(:expense) { { "some" => "data" } }
+
+        before do
+          allow(ledger).to receive(:record)
+            .with(expense)
+            .and_return(RecordResult.new(false, 417, "Expense incomplete"))
+        end
+
+        it "returns an error message" do
+          post "/expenses", JSON.generate(expense)
+
+          expect(parsed).to include("error" => "Expense incomplete")
+        end
+
+        it "response with a 422 (Unprocceseble entry)" do
+          post "expenses", JSON.generate(expense)
+          expect(last_response.status).to eq(422)
+        end
+      end
+    end
+
+    describe "GET /expenses/:date" do
+      context "when expenses exist on the given date" do
+        let(:expense) { { "some" => "data" } }
+        let(:date) { "2017-06-12" }
+
+        before do
+          allow(ledger).to receive(:expenses_on) 
+            .with(date) 
+            .and_return([expense])
+        end
+
+        it "returns the expense records as JSON" do
+          get "/expenses/2017-06-12"
+          expect(parsed).to include("some" => "data")
+        end
+
+        it "response with 200 (OK)" do
+          get "/expenses/2017-06-12"
+          expect(last_response.status).to eq(200)
+        end
+      end
+
+      context "when there are no expenses on the given date" do
+        let(:date) { "2017-06-12" }
+
+        before do
+          allow(ledger).to receive(:expenses_on)
+            .with(date)
+            .and_return([])
+        end
+
+        it "returns an empty array as JSON" do
+          get "/expenses/2017-06-12"
+          expect(parsed).to be_empty
+        end
+        it "response with 200 (OK)" do
+          get "/expenses/2017-06-12"
+          expect(last_response.status).to eq(200)
+        end
+      end
+    end
+  end
+end
